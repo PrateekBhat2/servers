1695f83d0cf2dc611a30f8ecd5309f9fb9a28a8c
Prateek Bhat <prateek.bhat@ukg.com>
2024-08-19 15:02:33 +0530

    add auth to gateway server

--- gatewayserver/src/main/java/com/ukg/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/ukg/gatewayserver/GatewayserverApplication.java
@@ -17,6 +17,15 @@ public class GatewayserverApplication {
 						.filters(f -> f.rewritePath("/payroll/payroll/(?<segment>.*)", "/${segment}"))
 						.uri("lb://PAYROLL")
 				).
+				route(p -> p
+						.path("/payroll/leave/**")
+						.filters(f -> f.rewritePath("/payroll/leave/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LEAVE")
+				).route(p -> p
+						.path("/payroll/employee/**")
+						.filters(f -> f.rewritePath("/payroll/employee/(?<segment>.*)", "/${segment}"))
+						.uri("lb://EMPLOYEE")
+				).
 				build();
 	}
 
---
+++ gatewayserver/src/main/java/com/ukg/gatewayserver/configuration/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.ukg.gatewayserver.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityWebFilterChain filterChain(ServerHttpSecurity httpSecurity){
+//
+//        httpSecurity.authorizeExchange(authorizeExchangeSpec ->
+//                authorizeExchangeSpec
+//                        .pathMatchers(HttpMethod.GET).permitAll()
+//                        .pathMatchers("/synergybank/accounts/**").authenticated()
+//                        .pathMatchers("/synergybank/cards/**").authenticated()
+//                        .pathMatchers("/synergybank/loans/**").authenticated()
+//
+//        ).oauth2ResourceServer(oAuth2ResourceServerSpec ->
+//                oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+//
+//        httpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+//
+//        return httpSecurity.build();
+//    }
+
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity httpSecurity){
+
+        httpSecurity.authorizeExchange(authorizeExchangeSpec ->
+                authorizeExchangeSpec
+                        .pathMatchers(HttpMethod.GET).permitAll()
+                        .pathMatchers("/payroll/employee/**").hasRole("EMPLOYEE")
+                        .pathMatchers("/payroll/payroll/**").hasRole("PAYROLL")
+                        .pathMatchers("/payroll/leave/**").hasRole("LEAVE")
+
+        ).oauth2ResourceServer(oAuth2ResourceServerSpec ->
+                oAuth2ResourceServerSpec.jwt(jwtSpec ->
+                        jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
+
+        httpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+
+        return httpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor(){
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
+    }
+}
